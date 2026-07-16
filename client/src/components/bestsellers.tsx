3ef515a3b978c62240bcccde3a713e169f178caa
Senidu Ravihara <senidumee16@gmail.com>
2026-07-16 22:19:14 +0530

    change

--- client/src/components/bestsellers.tsx
+++ client/src/components/bestsellers.tsx
@@ -5,6 +5,7 @@ import { CourseCard } from '@/components/course-card'
 import { Button } from '@/components/ui/button'
 
 const bestsellers = [
+  
   {
     id: '5',
     title: 'New-In LMS System',
